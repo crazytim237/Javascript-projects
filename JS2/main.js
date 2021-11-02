5ca41321507f571b1133e09b455264c8f169ead7
crazytim237 <87796455+crazytim237@users.noreply.github.com>
2021-11-02 15:29:01 -0400

    update

--- JS2/main.js
+++ JS2/main.js
@@ -1,3 +1,10 @@
 var [a , b] = ["one", "two"];
 document.getElementById("demo");
 document.write(a);
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+
