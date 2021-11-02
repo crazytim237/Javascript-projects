c9a4ba739723873b7fb3433e157b1beda8d5f40f
crazytim237 <87796455+crazytim237@users.noreply.github.com>
2021-11-01 21:55:53 -0400

    Project2

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-
-
-var A = "this is a string";
-document.write(A)
-
-
-document.write("\"be who you are and say what you feel," + "because those who dont mind don\'t matter abd tgise who matter don\'t mind.\"");
-
-var B = "Concatenated" + " string"
-
-var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "penny", Son = "zorrow";
-document.write(Dad);
-
-var blues = "I have the blues.";
-var blues = blues.fontcolor("blue");
-document.write(blues);
-
-document.write(3 + 3);
-
-function My_First_Function() { //defining a function and naming it
-                                      //defining a variable and giving it a string value
-     var str = "this text is green!"; //using the fontcolor method on str variable
-
-    var result = str.fontcolor("green").innerHTML = result; //putting the value of result into HTML element with "green_text" id
-}
+var [a , b] = ["one", "two"];
+document.getElementById("demo");
+document.write(a);
