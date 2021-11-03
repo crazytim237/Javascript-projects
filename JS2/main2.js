24217afb0ab7c2be38b6ae3c19a34fae6b2b083e
crazytim237 <87796455+crazytim237@users.noreply.github.com>
2021-11-03 06:06:48 -0400

    update

---
+++ JS2/main2.js
@@ -0,0 +1,10 @@
+var [a , b] = ["one", "two"]; // assigning two variables
+document.getElementById("demo");
+document.write(a); //displays the variable asked for
+
+function myFunction() {
+    var sentence = "I am learning"; //assigning a variable
+    sentence += " a lot from this book"; //adds the value of the right operand to a variable and assigns the result to the variable
+    document.getElementById("concatenate").innerHTML = sentence; //puts the two variables together
+}
+
