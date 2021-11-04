407891d7f1451374204a375da33e858c694e0162
crazytim237 <87796455+crazytim237@users.noreply.github.com>
2021-11-04 15:50:48 -0400

    update

--- JS2/main2.js
+++ JS2/main2.js
@@ -5,6 +5,10 @@ document.write(a); //displays the variable asked for
 function myFunction() {
     var sentence = "I am learning"; //assigning a variable
     sentence += " a lot from this book"; //adds the value of the right operand to a variable and assigns the result to the variable
-    document.getElementById("concatenate").innerHTML = sentence; //puts the two variables together
+    document.getElementById("Concatenate").innerHTML = sentence; //puts the two variables together
 }
 
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("math").innerHTML = "5 - 2 = " + Subtraction
+}
